2a5ff54fd00ad84cf8d9197a23a7cb167b822153
BasileBux <basile.buxtorf@ik.me>
2024-10-13 22:46:58 +0200

    save

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
@@ -22,10 +25,14 @@ var lightTheme Theme = Theme{
 }
 
 type WindowStyle struct {
-	PaddingTop    float64
-	PaddingRight  float64
-	PaddingBottom float64
-	PaddingLeft   float64
+	PaddingTop    float32
+	PaddingRight  float32
+	PaddingBottom float32
+	PaddingLeft   float32
+	Font          rl.Font
+	FontSize      float32
+	CursorOffset  int // horizontal distance to text
+	CursorWidth   int
 	ColorTheme    Theme
 }
 
@@ -34,10 +41,18 @@ var compact WindowStyle = WindowStyle{
 	PaddingRight:  13.0,
 	PaddingBottom: 13.0,
 	PaddingLeft:   13.0,
+	Font:          rl.Font{},
+	FontSize:      30,
+	CursorOffset:  3,
+	CursorWidth:   8,
 	ColorTheme:    darkTheme,
 }
 
-func inputManager(text *string) {
+type NavigationData struct {
+	SelectedLine int // 0 indexed
+}
+
+func inputManager(text *string, nav *NavigationData) {
 	char := rl.GetCharPressed()
 	for char > 0 {
 		// refuse non ascii and non printable chars
@@ -47,38 +62,79 @@ func inputManager(text *string) {
 		char = rl.GetCharPressed()
 	}
 
-	if rl.IsKeyPressedRepeat(rl.KeyBackspace) && len(*text) > 0 {
+	if (rl.IsKeyPressedRepeat(rl.KeyBackspace) || rl.IsKeyPressed(rl.KeyBackspace)) && len(*text) > 0 {
+		if (*text)[len(*text)-1] == '\n' {
+			nav.SelectedLine--
+		}
 		*text = (*text)[:len(*text)-1]
 	}
-	if rl.IsKeyPressed(rl.KeyEnter) {
+
+	if rl.IsKeyPressedRepeat(rl.KeyEnter) || rl.IsKeyPressed(rl.KeyEnter) {
 		*text += "\n"
+		nav.SelectedLine++
 	}
 }
 
+func getTextSize(text *string, nav *NavigationData, style *WindowStyle) (rl.Vector2, error) {
+	lines := strings.Split(*text, "\n")
+	if nav.SelectedLine > len(*text)-1 {
+		fmt.Println("Error, navigation index is too big!")
+		return rl.Vector2{}, fmt.Errorf("Navigation index to big\n")
+	}
+	currentLine := strings.Split(lines[nav.SelectedLine], "\n")
+	textSize := rl.MeasureTextEx(style.Font, currentLine[0], style.FontSize, 1)
+	return textSize, nil
+}
+
 func main() {
+	logFile, err := os.OpenFile("raylib.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer logFile.Close()
+	rl.SetTraceLogCallback(func(msgType int, text string) {
+		fmt.Fprintf(logFile, "%s\n", text)
+	})
+
 	rl.InitWindow(1000, 800, "My custom text editor")
 	if !rl.IsWindowReady() {
 		log.Panic("Window didn't open correctly ???")
 	}
 	defer rl.CloseWindow()
-	userFont := rl.LoadFontEx("/usr/share/fonts/GeistMono/GeistMonoNerdFont-Regular.otf", 100, nil)
-	rl.SetTextureFilter(userFont.Texture, rl.FilterBilinear)
-	rl.SetTargetFPS(144)
 
 	userStyle := compact
+	userStyle.Font = rl.LoadFontEx("/usr/share/fonts/GeistMono/GeistMonoNerdFont-Regular.otf", 100, nil)
+	rl.SetTextureFilter(userStyle.Font.Texture, rl.FilterBilinear)
+	rl.SetTargetFPS(144)
+
 	var userText string
+	nav := NavigationData{
+		SelectedLine: 0,
+	}
 
 	for !rl.WindowShouldClose() {
 
-		inputManager(&userText)
+		inputManager(&userText, &nav)
 
 		rl.BeginDrawing()
 
 		rl.ClearBackground(userStyle.ColorTheme.Background)
-		textPos := rl.NewVector2(float32(userStyle.PaddingLeft), float32(userStyle.PaddingTop))
-		rl.DrawTextEx(userFont, userText, textPos, 30, 1, userStyle.ColorTheme.Text)
+
+		textPos := rl.NewVector2(userStyle.PaddingLeft, userStyle.PaddingTop)
+		rl.DrawTextEx(userStyle.Font, userText, textPos, userStyle.FontSize, 1, userStyle.ColorTheme.Text)
+
+		// Draw cursor rectangle
+		// TODO: This gives error of nav index being wrong. Check this out
+		textSize, err := getTextSize(&userText, &nav, &userStyle)
+		if err != nil {
+			fmt.Println("Resetting nav index")
+			nav.SelectedLine = 0
+		}
+		// TODO: Show cursor when there is no text
+		rl.DrawRectangle(int32(userStyle.PaddingLeft)+int32(textSize.X)+int32(userStyle.CursorOffset), int32(userStyle.PaddingTop)+int32(nav.SelectedLine)*int32(textSize.Y)+int32(nav.SelectedLine+1), int32(userStyle.CursorWidth), int32(textSize.Y), rl.RayWhite)
 
 		rl.EndDrawing()
 	}
-	rl.UnloadFont(userFont)
+	rl.UnloadFont(userStyle.Font)
 }
